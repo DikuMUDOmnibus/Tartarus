182df9ce1bcf5908909288cd189716ffea1ab913
David Reynolds <david@alwaysmovefast.com>
2011-04-30 18:51:42 -0700

    added two new NPCs. I'd like to have a single file for them

---
+++ data/npcs/demon.js
@@ -0,0 +1,10 @@
+{
+    "area_id": 0,
+    "room_id": 2,
+    "name": "Fire Demon",
+    "color": "&R",
+    "ch_state": 4,
+    "is_mobile": 0,
+    "inventory": [],
+    "keywords": ["fire", "demon", "fire demon"]
+}
---
+++ data/npcs/gatekeeper.js
@@ -0,0 +1,10 @@
+{
+    "area_id": 0,
+    "room_id": 0,
+    "name": "Tartarus Gatekeeper",
+    "color": "&R",
+    "ch_state": 4,
+    "is_mobile": 0,
+    "inventory": [],
+    "keywords": ["gatekeeper"]
+}
