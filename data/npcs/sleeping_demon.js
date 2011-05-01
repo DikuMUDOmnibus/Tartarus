a04a00353ac8ecb8c3e2a6c05fdecebfd85909bd
David Reynolds <david@alwaysmovefast.com>
2011-04-30 21:10:24 -0700

    adding new demon type

---
+++ data/npcs/sleeping_demon.js
@@ -0,0 +1,10 @@
+{
+    "area_id": 0,
+    "room_id": 7,
+    "name": "Lesser Demon",
+    "color": "&R",
+    "ch_state": 2,
+    "is_mobile": 0,
+    "inventory": [],
+    "keywords": ["sleeping", "demon", "sleeping demon"]
+}
