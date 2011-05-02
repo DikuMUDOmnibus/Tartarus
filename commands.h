a36a1a3a1edbd87d1787bebf23ed67233a5e8c60
David Reynolds <david@alwaysmovefast.com>
2011-05-02 14:20:55 -0700

    updated license

--- commands.h
+++ commands.h
@@ -12,7 +12,7 @@
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
  *
- *   3. Neither the name of the Owner nor the names of its contributors
+ *   3. Neither the name of Tartarus nor the names of its contributors
  *      may be used to endorse or promote products derived from this software
  *      without specific prior written permission.
  *
